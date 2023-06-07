ca34cf57d24443b15e8fc02ac36b9239a26ce049
Vincent Maglione <vincent@bluebench.co>
2023-06-06 22:24:18 -0400

    Daily report model, migration

---
+++ app/Models/ScDailyReport.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ScDailyReport extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'period_start_at' => 'immutable_datetime',
+        'period_end_at' => 'immutable_datetime',
+        'day' => 'immutable_datetime',
+        'alert_kwh' => 'float',
+        'average_daily_cost_usd' => 'float',
+        'overage_low_kwh' => 'float',
+        'overage_high_kwh' => 'float',
+        'weekday_cost_usd' => 'float',
+        'weekday_usage_kwh' => 'float',
+        'weekend_cost_usd' => 'float',
+        'weekend_usage_kwh' => 'float',
+        'temp_high_f' => 'float',
+        'temp_low_f' => 'float',
+    ];
+
+    public function scAccount(): BelongsTo
+    {
+        return $this->belongsTo(ScAccount::class);
+    }
+}
---
+++ database/migrations/2023_06_07_014545_create_sc_daily_reports_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\ScAccount;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sc_daily_reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(ScAccount::class);
+            $table->timestamp('period_start_at')->index();
+            $table->timestamp('period_end_at')->index();
+            $table->timestamp('day')->index();
+            $table->float('alert_kwh')->nullable();
+            $table->float('average_daily_cost_usd')->nullable();
+            $table->float('overage_low_kwh')->nullable();
+            $table->float('overage_high_kwh')->nullable();
+            $table->float('weekday_cost_usd')->nullable();
+            $table->float('weekday_usage_kwh')->nullable();
+            $table->float('weekend_cost_usd')->nullable();
+            $table->float('weekend_usage_kwh')->nullable();
+            $table->float('temp_high_f')->nullable();
+            $table->float('temp_low_f')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sc_daily_reports');
+    }
+};
